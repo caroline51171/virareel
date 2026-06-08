4869d0ab3e5d46c97da6c52fb91b3ac5c7932909
caroline51171 <caroline51171@hotmail.fr>
2026-06-08 17:54:26 -0400

    Fix UserButton props Clerk

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
                 </button>
               </SignInButton>
             ) : (
-              <UserButton afterSignOutUrl="/" />
+              <UserButton />
             )}
           </div>
         </div>
